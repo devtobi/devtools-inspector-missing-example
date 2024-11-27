4d841a6cc83c8ff833c4298985a4045934a23451
devtobi <28538704+devtobi@users.noreply.github.com>
2024-11-27 09:59:48 +0100

    Added minimal typescript based example

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// Plugins
+import { fileURLToPath, URL } from "node:url";
+
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      features: {
+        optionsAPI: true,
+      },
+    }),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
